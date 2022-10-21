23acf71286ecf10186af4dbedee161111a3ba049
Aleksandr Sidorov <alexander.a.sidorov@gmail.com>
2022-10-21 16:38:46 +0300

    Update Lesson_07.py

--- scripts/Lesson_07.py
+++ scripts/Lesson_07.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Oct 17 14:03:43 2022
-
-@author: sidorow
-"""
-# Библиотека Pandas
-"""
-Библиотека Pandas - является фактически является заменой электронной таблицы 
+Библиотека Pandas
+Библиотека Pandas - является фактически является заменой электронной таблицы
 типа Excel и позволяет сортировать и "чистить" большие массивы данных.
 В данном уроке мы попробуем рассмотреть базовые команды библиотеки на примере
-работы с данными экспериментального графика напряжение-формация  
-"""
+работы с данными экспериментального графика напряжение-формация"""
 ## Для начала импортируем нужные библиотеки
 import pandas as pd # библиотека Pandas
 import numpy as np # библиотека NumPy (см. урок 06)
@@ -18,56 +11,55 @@ import matplotlib.pyplot as pl # библиотека Matplotlib (см. урок
 import os # библиотека для перемещения между директориями
 os.chdir('../data') # переходим в корневую папку проекта
 ## Считываем данные из Excel файла (первый лист)
-FirstSheet=pd.read_excel('Lesson07.xlsx', header=None, 
-                             sheet_name='Results')
+First_Sheet = pd.read_excel ('Lesson07.xlsx', header=None, sheet_name='Results')
 # Считываем второй лист
-SecondSheet=pd.read_excel('Lesson07.xlsx', header=None, 
-                             sheet_name='Data')
-os.chdir('../scripts') # возвращаемся впапку scripts 
+Second_Sheet = pd.read_excel('Lesson07.xlsx', header=None, sheet_name='Data')
+os.chdir('../scripts') # возвращаемся впапку scripts
 ## Далее мы будем работать со сторым листом, где приведены эксперементальные
-## данные напряжение-деформация. Создадим отдельную переменную FlowStressData,
-## с которой будем дальше работать, а переменную SecondSheet оставим в качестве
+## данные напряжение-деформация. Создадим отдельную переменную FlowStress_Data,
+## с которой будем дальше работать, а переменную Second_Sheet оставим в качестве
 ## референсной
-FlowStressData=SecondSheet
+FlowStress_Data=Second_Sheet
 # Для начала посмотрим, что именно мы считали с помощью команды describe
-Description=FlowStressData.describe()
+Description=FlowStress_Data.describe()
 # здесь count - количество строк, unique - количество строк с уникальными значениями
 # top  - максимально часто встречающееся в столбце значение, freq - количество
 # повторений этого значения.
 # Далее определим размер массива данных:
-ShapeFlowStressData=FlowStressData.shape
+ShapeFlowStress_Data=FlowStress_Data.shape
 # Нам необходимы данные из первых двух столбцов (Инженерная деформация - столбец 0
 # и инженерное напряжение - столбец 1)
-for i in range (2, ShapeFlowStressData[1]):
-    FlowStressData.drop([i], axis=1, inplace=True)
+for i in range (2, ShapeFlowStress_Data[1]):
+    FlowStress_Data.drop([i], axis=1, inplace=True)
 del i
 # Т.к. первые три строки нам так же не нужны, их мы удалим аналогичным образом
-FlowStressData.drop([0, 1, 2], axis=0, inplace=True)
+FlowStress_Data.drop([0, 1, 2], axis=0, inplace=True)
 # ОБратите внимание, что удалив первые три строки мы сломали индексацию, теперь
 # первым индексом у нас выступает число 3. Это можно поправить:
-FlowStressData=FlowStressData.reset_index(drop=True)
+FlowStress_Data=FlowStress_Data.reset_index(drop=True)
 # здесь с помощью атрибута drop=True мы даем понять, что не надо создавать новый
 # столбец для индекса, а нужно просто записать новые индексы в старый
 # Для удобства можем дать столбцам наименования:
-FlowStressData.columns=['EngineeringStrain', 'EngineeringStress']
+FlowStress_Data.columns=['EngineeringStrain', 'EngineeringStress']
 # Мы можем тперь, например, определить предел прочности для этого материала
-MaxTensileStress=FlowStressData['EngineeringStress'].max()
+Max_Tensile_Stress=FlowStress_Data['EngineeringStress'].max()
 # Можем конфертировать данные из этих столбцов в NumPy array, что бы далее было
 # проще с ними работать:
-FlowStressDataNumPy=FlowStressData.to_numpy(dtype=float)
+FlowStress_DataNumPy=FlowStress_Data.to_numpy(dtype=float)
 # и записать их обратно в массив Pandas
-FlowStressData['EngineeringStrain']=FlowStressDataNumPy[:,0]
-FlowStressData['EngineeringStress']=FlowStressDataNumPy[:,1]
+FlowStress_Data['EngineeringStrain']=FlowStress_DataNumPy[:,0]
+FlowStress_Data['EngineeringStress']=FlowStress_DataNumPy[:,1]
 # Пересчитаем первый столбец из деформации в процентах в деформацию в долях от 1
-FlowStressData['EngineeringStrain']=FlowStressData['EngineeringStrain']/100
+FlowStress_Data['EngineeringStrain']=FlowStress_Data['EngineeringStrain']/100
 # После чего добавить столбцы с расчетными значениями истинных напряжений и дефомраций
-FlowStressData['TrueStrain']=np.log(1+FlowStressData['EngineeringStrain'])
-FlowStressData['TrueStress']=FlowStressData['EngineeringStress']*(1+FlowStressData['EngineeringStrain'])
-# 
+FlowStress_Data['TrueStrain']=np.log(1+FlowStress_Data['EngineeringStrain'])
+FlowStress_Data['TrueStress']=FlowStress_Data['EngineeringStress']*(1+
+    FlowStress_Data['EngineeringStrain'])
 #
-fig01=pl.figure(1)
-pl.plot(FlowStressData['EngineeringStrain'], FlowStressData['EngineeringStress'], '-*k')
-pl.plot(FlowStressData['TrueStrain'], FlowStressData['TrueStress'], '-or')
+#
+Fig_01=pl.figure(1)
+pl.plot(FlowStress_Data['EngineeringStrain'], FlowStress_Data['EngineeringStress'], '-*k')
+pl.plot(FlowStress_Data['TrueStrain'], FlowStress_Data['TrueStress'], '-or')
 #
 # Добавим названия осей:
 pl.xlabel('Деформация')
@@ -75,40 +67,40 @@ pl.ylabel('Напряжение, МПа')
 #
 #%%
 # Далее будем работать с графиком Истанных напряжений и деформаций
-# Для этого создадим переменную TrueStressStrain в формате NumPy array
-TrueStressStrain=np.array([FlowStressData['TrueStrain'], FlowStressData['TrueStress']])
-TrueStressStrain=np.transpose(TrueStressStrain)
+# Для этого создадим переменную True_Stress_Strain в формате NumPy array
+True_Stress_Strain=np.array([FlowStress_Data['TrueStrain'], FlowStress_Data['TrueStress']])
+True_Stress_Strain=np.transpose(True_Stress_Strain)
 # Определим модуль Юнга для этого материала
 # Для этого нам нужно выделить заведомо упругую часть массива
 ElasticStrain = np.array([0.0, 0.0])
 ElasticStress =np.array ([0.0, 0.0])
 i=0
 while ElasticStrain[1] < 0.001:
-    ElasticStrain[1]=TrueStressStrain[i,0]
-    ElasticStress[1]=TrueStressStrain[i,1]
+    ElasticStrain[1]=True_Stress_Strain[i,0]
+    ElasticStress[1]=True_Stress_Strain[i,1]
     i=i+1
 del i
 # По закону Гука Sigma = E * Epsilon или E = Sigma/Epsilon, где E - модуль Юнга
 # отсюда Модуль Юнга
 E=ElasticStress[1]/ElasticStrain[1]
-# Определим предел текучести из условия, что остаточная деформация больше или 
+# Определим предел текучести из условия, что остаточная деформация больше или
 # равна 0.002
 i=0
 Delta = 0
 while Delta <= 0.002:
-    Delta=TrueStressStrain[i,0]-TrueStressStrain[i,1]/E
-    Sigma02=TrueStressStrain[i,1]
+    Delta=True_Stress_Strain[i,0]-True_Stress_Strain[i,1]/E
+    Sigma_02=True_Stress_Strain[i,1]
     i=i+1
-# Найдем индекс массива TrueStressStrain, где напряжение максимально (т.е. найдем
+# Найдем индекс массива True_Stress_Strain, где напряжение максимально (т.е. найдем
 # момент начала образования шейки)
-j=TrueStressStrain[:,1].argmax()
+j=True_Stress_Strain[:,1].argmax()
 # Удаляем все данные до предела текучести и после начала образования шейки
-PlasticStrain=TrueStressStrain[(i-1):j, 0]
-PlasticStrain=PlasticStrain-PlasticStrain[0]
-PlasticStress=TrueStressStrain[(i-1):j, 1]
+Plastic_Strain=True_Stress_Strain[(i-1):j, 0]
+Plastic_Strain=Plastic_Strain-Plastic_Strain[0]
+Plastic_Stress=True_Stress_Strain[(i-1):j, 1]
 # Построим график пластическая деформация - напряжение
-fig02=pl.figure(2)
-pl.plot(PlasticStrain, PlasticStress, 'r')
+Fig_02=pl.figure(2)
+pl.plot(Plastic_Strain, Plastic_Stress, 'r')
 # Добавим названия осей:
 pl.xlabel('Пластическая деформация')
 pl.ylabel('Напряжение, МПа')
@@ -116,65 +108,45 @@ pl.ylabel('Напряжение, МПа')
 # Для этого нам понадобится библиотека SciPy
 from scipy.optimize import curve_fit
 # и определим уравнение, которым мы будем апроксимировать, с помощью функции
-def FlowCurve (XData,K, n):
-    return Sigma02+K*XData**n
-XData=PlasticStrain
-YData=PlasticStress
-initialGuess = [0,2]
-popt, pcov = curve_fit(FlowCurve, XData, YData, initialGuess)
+def flow_curve (X_Data,K, n):
+    return Sigma_02+K*X_Data**n
+X_Data=Plastic_Strain
+Y_Data=Plastic_Stress
+Initial_Guess = [0,2]
+popt, pcov = curve_fit(flow_curve, X_Data, Y_Data, Initial_Guess)
 K=popt[0]
 n=popt[1]
-fig03=pl.figure(3)
-pl.plot(PlasticStrain, PlasticStress, 'r')
-pl.plot(PlasticStrain, FlowCurve(PlasticStrain, K, n), 'b')
+Fig_03=pl.figure(3)
+pl.plot(Plastic_Strain, Plastic_Stress, 'r')
+pl.plot(Plastic_Strain, flow_curve(Plastic_Strain, K, n), 'b')
 # Добавим названия осей:
 pl.xlabel('Пластическая деформация')
 pl.ylabel('Напряжение, МПа')
-#%% Т.к. результат апроксимации далек от идеального, попробуем апроксимировать
+    #%% Т.к. результат апроксимации далек от идеального, попробуем апроксимировать
 # эти же данные простым полиномом третьего порядка
-def Polynom (x, a, b, c, d):
+def polynom (x, a, b, c, d):
     return a*x**3+b*x**2+c*x+d
-initialGuess = [1,1,1,1]
-popt, pcov = curve_fit(Polynom, XData, YData, initialGuess)
+Initial_Guess = [1,1,1,1]
+popt, pcov = curve_fit(polynom, X_Data, Y_Data, Initial_Guess)
 constants=popt
-fig04=pl.figure(4)
-pl.plot(PlasticStrain, PlasticStress, 'r')
-pl.plot(PlasticStrain, Polynom(PlasticStrain, constants[0], constants[1], 
+Fig_04=pl.figure(4)
+pl.plot(Plastic_Strain, Plastic_Stress, 'r')
+pl.plot(Plastic_Strain, polynom(Plastic_Strain, constants[0], constants[1],
                                constants[2], constants[3]), 'b')
 # # # Добавим названия осей:
 pl.xlabel('Пластическая деформация')
 pl.ylabel('Напряжение, МПа')
-#%% Если хотим получить совсем идеальный результат, то можем попробовать 
+#%% Если хотим получить совсем идеальный результат, то можем попробовать
 # увеличить размерность полинома
-def Polynom (x, a, b, c, d, e):
+def polynom (x, a, b, c, d, e):
     return a*x**4+b*x**3+c*x**2+d*x+e
-initialGuess = [1,1,1,1, 1]
-popt, pcov = curve_fit(Polynom, XData, YData, initialGuess)
+Initial_Guess = [1, 1, 1, 1, 1]
+popt, pcov = curve_fit(polynom, X_Data, Y_Data, Initial_Guess)
 constants=popt
-fig05=pl.figure(5)
-pl.plot(PlasticStrain, PlasticStress, 'r')
-pl.plot(PlasticStrain, Polynom(PlasticStrain, constants[0], constants[1], 
+Fig_05=pl.figure(5)
+pl.plot(Plastic_Strain, Plastic_Stress, 'r')
+pl.plot(Plastic_Strain, polynom(Plastic_Strain, constants[0], constants[1],
                                constants[2], constants[3], constants[4]), 'b')
 # # # Добавим названия осей:
 pl.xlabel('Пластическая деформация')
 pl.ylabel('Напряжение, МПа')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
